fe37b3202a2c1ab419959e001c5b943e0f0091c7
beamsbe97 <beamsbe97@github.com>
2025-10-20 23:09:32 +0200

    added gitignore

--- task1.py
+++ task1.py
@@ -13,7 +13,7 @@ transform = transforms.Compose(
     transforms.Normalize((0.5,),(0.5,))]
 )
 
-batch_size = 4
+batch_size = 64
 
 trainset = torchvision.datasets.FashionMNIST(root='./data', train=True, 
                                             download=True, transform=transform)
